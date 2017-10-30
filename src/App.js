f4e2ca677bca8e6e8c3b4afd3993bbfe599355ab
pranavj1001 <pranavj1001@gmail.com>
2017-10-30 19:50:37 +0530

    add Header to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <Text> Hey There </Text>
+        <Header headerText="Authentication" />
       </View>
     );
   }
