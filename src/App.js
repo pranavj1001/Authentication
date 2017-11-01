fbfdb7b908c6497df3334c01f2f820ab48795153
pranavj1001 <pranavj1001@gmail.com>
2017-11-01 11:39:18 +0530

    add LoginForm Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      // fill in the firebase setup details here
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
