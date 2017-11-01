342139260ac5ad0f21c5bfd721711c2481ccfa2c
pranavj1001 <pranavj1001@gmail.com>
2017-11-01 11:49:54 +0530

    add textInput for getting email

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  state = { email: '' };
+
   render() {
     return (
       <Card>
         <CardSection>
+          <TextInput
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            style={{ height: 20, width: 100 }}
+          />
         </CardSection>
-        
+
+        <CardSection>
+        </CardSection>
+
         <CardSection>
           <Button>
             Login
