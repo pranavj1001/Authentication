2ea5b24bf55b6f54b1db0b87b58e5767d1767d4d
pranavj1001 <pranavj1001@gmail.com>
2017-11-04 11:27:15 +0530

    set states for authenticated users

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false }
+
   componentWillMount() {
     firebase.initializeApp({
       // fill in the firebase setup details here
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
